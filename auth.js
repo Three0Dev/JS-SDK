14327a6defdec9e43eff543ef1f3c528bc3a02a5
Sree Grandhe <sgrandhe@utexas.edu>
2022-01-09 00:02:34 -0600

    Added inclusion of userExists function to avoid useless user creation

--- auth.js
+++ auth.js
@@ -1,4 +1,5 @@
 export class Auth {
+    
     constructor(config) {
         this.config = config;
     }
@@ -9,20 +10,23 @@ export class Auth {
         window.location.replace(window.location.origin + window.location.pathname)
       }
       
-    login() {
+    async login() {
         // Allow the current app to make calls to the specified contract on the
         // user's behalf.
         // This works by creating a new access key for the user's account and storing
         // the private key in localStorage.
         window.walletConnection.requestSignIn(this.config.contractName)
-        try {
-            await window.contract.createUser({
-                pid: this.config.pid,
-                orbitID: "" // TODO
-            });
-        } catch (e) {
-            console.log(e)
-            throw e
+
+        if(!(await window.contract.userExists())) {
+            try {
+                await window.contract.createUser({
+                    pid: this.config.pid,
+                    orbitID: "" // TODO
+                });
+            } catch (e) {
+                console.error(e)
+                throw e
+            }
         }
     }
 }
