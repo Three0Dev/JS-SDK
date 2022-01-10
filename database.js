98e4417a7c51d18a1a46ea4b745d406082a57454
Sree Grandhe <sgrandhe@utexas.edu>
2022-01-09 20:43:34 -0600

    Connect NEAR wallet with OrbitDB credentials

--- database.js
+++ database.js
@@ -1,17 +1,21 @@
 import { IPFS } from 'ipfs';
 import { OrbitDB } from 'orbit-db';
 import { PID } from './config';
+import { IdentityProvider, Identities } from "orbit-db-identity-provider";
+import { keyStores } from 'near-api-js';
+
+
+Identities.addIdentityProvider(NearIdentityProvider)
 
 export class Database {
     constructor() {
         // OrbitDB instance
         this.orbitdb = null
-        // Databases
-        this.#startDBServices()
     }
 
-    #startDBServices = async () => {
-        await this.#initOrbitDB(await this.#initIPFS())
+    initDB = async () => {
+        const identity = await Identities.createIdentity({ type: `NearIdentity`})
+        await this.#initOrbitDB(await this.#initIPFS() , {identity})
     }
 
     // Start IPFS
@@ -35,6 +39,10 @@ export class Database {
             throw new Error("DB is not initialized")
         }
 
+        if(!window.walletConnection.isSignedIn()){
+            throw new Error("User not authenticated")
+        }
+
         const address = (await window.contract.getDatabaseAddress({
             pid: PID,
             name: name
@@ -46,4 +54,27 @@ export class Database {
         return db
     }
 
+}
+
+class NearIdentityProvider extends IdentityProvider {
+    // return type
+    static get type () {
+         return 'NearIdentity' 
+    }
+    // return identifier of external id (eg. a public key)
+    async getId () { 
+        return window.accountId
+    } 
+    //return a signature of data (signature of the OrbitDB public key)
+    async signIdentity (data) {
+        const keyStore = new keyStores.BrowserLocalStorageKeyStore()
+        const keyPair = await keyStore.getKey(NEAR_CONFIG.networkId, window.accountId)
+        return keyPair.sign(data).signature
+    }
+    //return true if identity.signatures are valid 
+    static async verifyIdentity (identity) { 
+        const keyStore = new keyStores.BrowserLocalStorageKeyStore()
+        const keyPair = await keyStore.getKey(NEAR_CONFIG.networkId, window.accountId)
+        return keyPair.verify(identity.publicKey + identity.signatures.id, identity.signatures.publicKey)
+    }
 }
