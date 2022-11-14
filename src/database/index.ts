8070cbc2144480159fc4d37414f07e7a1e29a079
Harshit Gupta <hersheyg0105@gmail.com>
2022-11-14 01:21:03 -0600

    Adding headers to database
    Added function headers for all in src/database/wrappers

--- src/database/index.ts
+++ src/database/index.ts
@@ -27,6 +27,14 @@ enum DatabaseType {
 	KeyValueDB = 'keyvalue',
 }
 
+/**
+ * This function creates a specified database type and returns it
+ * 
+ * @param address Database address
+ * @param type Database type
+ * @param options Database additional parameters
+ * @returns New database object
+ */
 async function getDB(
 	address: string,
 	type: string,
@@ -65,11 +73,23 @@ async function getDB(
 	return db
 }
 
+/**
+ * This function creates a new counter database
+ * 
+ * @param address Database address
+ * @returns Counter database
+ */
 export async function Counter(address: string): Promise<CounterDatabase> {
 	const db = await getDB(address, DatabaseType.CounterDB)
 	return db as CounterDatabase
 }
 
+/**
+ * This function creates a new document database
+ * 
+ * @param address Database address
+ * @returns Document database
+ */
 export async function DocStore(
 	address: string,
 	options: DocStoreOptions = { indexBy: '_id' }
@@ -78,21 +98,44 @@ export async function DocStore(
 	return db as DocumentDatabase
 }
 
+/**
+ * This function creates a new event log database
+ * 
+ * @param address Database address
+ * @returns Event log database
+ */
 export async function EventLog(address: string): Promise<EventLogDatabase> {
 	const db = await getDB(address, DatabaseType.EventLogDB)
 	return db as EventLogDatabase
 }
 
+/**
+ * This function creates a new feed database
+ * 
+ * @param address Database address
+ * @returns Feed database
+ */
 export async function Feed(address: string): Promise<FeedDatabase> {
 	const db = await getDB(address, DatabaseType.FeedDB)
 	return db as FeedDatabase
 }
 
+/**
+ * This function creates a new key value database
+ * 
+ * @param address Database address
+ * @returns Key value database
+ */
 export async function KeyValue(address: string): Promise<KVDatabase> {
 	const db = await getDB(address, DatabaseType.KeyValueDB)
 	return db as KVDatabase
 }
 
+/**
+ * This function returns the current time
+ * 
+ * @returns Current time
+ */
 export function timestamp() {
 	return Date.now()
 }
--- src/database/wrappers/database.ts
+++ src/database/wrappers/database.ts
@@ -3,10 +3,20 @@ import Store from 'orbit-db-store'
 export default class Database {
 	protected readonly database: Store
 
+	/**
+	 * This function constructs a new database
+	 * 
+	 * @param database Database object
+	 */
 	constructor(database: Store) {
 		this.database = database
 	}
 
+	/**
+	 * This function updates information across all systems once database is updated
+	 * 
+	 * @param callbackfn Function
+	 */
 	onChange(callbackfn: Function) {
 		this.database.events.on('replicated', () => callbackfn())
 	}
